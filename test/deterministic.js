eac2c1e99b0f5a0b4adecc9783cd1b002a318c0b
Julian Gruber <julian@juliangruber.com>
2013-07-22 23:49:56 +0200

    passing test for deterministic behavior

---
+++ test/deterministic.js
@@ -0,0 +1,19 @@
+var level = require('level-test')({ mem: true });
+var Trie = require('..');
+var test = require('tape');
+var through = require('through');
+
+test('deterministic', function (t) {
+  t.plan(1);
+  var trie = Trie(level());
+
+  trie.add('foo');
+  trie.add('foo');
+
+  var res = [];
+  trie.createSearchStream('foo')
+    .pipe(through(write, end));
+
+  function write (str) { res.push(str) }
+  function end () { t.deepEqual(res, ['foo']) }
+});
