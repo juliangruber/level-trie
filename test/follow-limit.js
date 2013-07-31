b0d8c7d39d74c2f81b4d87fb1da1c8d7c1c1b624
Julian Gruber <julian@juliangruber.com>
2013-07-31 15:15:36 -0400

    added test for follow in conjunction with limit

---
+++ test/follow-limit.js
@@ -0,0 +1,24 @@
+var level = require('level-test')({ mem: true });
+var Trie = require('..');
+var test = require('tape');
+var through = require('through');
+
+test('follow with limit', function (t) {
+  t.plan(2);
+
+  var trie = Trie(level());
+  trie.add('bar');
+  var rs = trie.createSearchStream('fabulous', {
+    follow: true,
+    limit: 1
+  });
+
+  rs.once('data', function (str) {
+    t.equal(str, 'bar');
+    rs.once('data', function (str) {
+      t.equal(str, 'foo');
+    });
+    trie.add('aaa');
+    trie.add('foo');
+  });
+});
